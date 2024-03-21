cf8638f6bf70453115ad667a678101f342eaa1f3
Roney <ronaldorhoney@hotmail.com>
2024-03-21 15:10:49 -0300

    main

---
+++ variaveis.py
@@ -0,0 +1,27 @@
+nome = "Rhoney"
+print(nome)
+
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = "Rhoney", 37
+estado = True
+
+# função type()
+print(type(media))
+print(type(n2))
+print(type(nome))
+print(type(estado))
+print(type(1+2j))
+
+# função isinstance
+a = 10
+b = 'sol'
+print(isinstance(a, int))
+print(isinstance(b, str))
+print(isinstance(a, float))
+print(isinstance(a, (int, float)))
+
+a = 40
+c = 3
+r = a * c
+print(r)
